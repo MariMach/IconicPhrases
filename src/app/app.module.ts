5d57e84ab63100fca060f46a1226d78bb3a98bb2
MariamMach <39934426+MariamMach@users.noreply.github.com>
2020-03-27 23:22:48 +0100

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,30 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./post-create/post-create.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatSliderModule
+} from "@angular/material";
+import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  declarations: [AppComponent, PostCreateComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSliderModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
