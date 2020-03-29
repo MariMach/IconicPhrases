b299ac8a1e96e86714067db1acd970a07fd87f7d
MariamMach <39934426+MariamMach@users.noreply.github.com>
2020-03-29 02:29:29 +0100

    routes folder in backend

---
+++ backend/routes/posts.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const Post = require("./models/post");
+const router = express.Router();
+
+router.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Posts fetched succesfully!",
+      postId: createdPost._id
+    });
+  });
+});
+
+router.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({ _id: req.params.id }, post)
+    .then(result => {
+      console.log(result);
+      res.status(200).json({
+        message: "Post updated!"
+      });
+    })
+    .catch(() => {
+      console.log("something went wrong with the update");
+    });
+});
+
+router.get("/api/posts", (req, res, next) => {
+  Post.find()
+    .then(doc => {
+      res.status(200).json({
+        message: "Posts fetched succesfully!",
+        posts: doc
+      });
+    })
+    .catch(() => {
+      console.log(" posts find error");
+    });
+});
+
+router.get("/api/posts/:id", (req, res, next) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: "Post not found !",
+          post: post
+        });
+      }
+    })
+    .catch(() => {
+      console.log(" posts find error");
+    });
+});
+
+router.delete("/api/posts/:id", (req, res, next) => {
+  Post.findByIdAndDelete(req.params.id)
+    .then(result => {
+      console.log(result);
+      res.status(200).json({
+        message: "Post deleted!"
+      });
+    })
+    .catch(() => {
+      console.log("something went wrong");
+    });
+});
+
+module.exports = router;
