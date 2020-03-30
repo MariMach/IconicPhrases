f8f9c510bf3ca29d5fadb52939efacea782b5848
MariamMach <39934426+MariamMach@users.noreply.github.com>
2020-03-30 02:29:06 +0100

    make images backend folder accessible

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const postsRoutes = require("./routes/posts");
+const path = require("path");
 const app = express();
 
 mongoose
@@ -19,6 +20,8 @@ mongoose
 // parsing different bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+// images folder accessible
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
